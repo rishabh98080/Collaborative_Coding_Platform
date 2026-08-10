1c3962e2f0c393f35eb034157048359962b88fab
rishabh98080 <rishabh24273239pandey@gmail.com>
2026-08-11 00:53:56 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/antigravity/collaborativecoding/config/CorsConfig.java
+++ backend/src/main/java/com/antigravity/collaborativecoding/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.antigravity.collaborativecoding.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
