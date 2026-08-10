f271fb28a6a8cac437087857808258d44c7cebf2
rishabh98080 <rishabh24273239pandey@gmail.com>
2026-08-11 00:36:56 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/antigravity/collaborativecoding/config/CorsConfig.java
+++ backend/src/main/java/com/antigravity/collaborativecoding/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:3001","https://collaborative-coding-platform-f9sx.onrender.com")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3001","https://collaborative-coding-platform-arteuoond.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
