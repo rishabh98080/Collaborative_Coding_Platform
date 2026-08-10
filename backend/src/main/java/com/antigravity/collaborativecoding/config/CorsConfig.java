77ef7b98b5d1510ef4625c8e2d13ff2dc82817cc
rishabh98080 <rishabh24273239pandey@gmail.com>
2026-08-11 00:12:44 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/antigravity/collaborativecoding/config/CorsConfig.java
+++ backend/src/main/java/com/antigravity/collaborativecoding/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:3001","https://collaborative-coding-platform-arteound.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
