124990bda4f38f31f7095090b94d1a927062328b
rishabh98080 <rishabh24273239pandey@gmail.com>
2026-08-11 00:57:05 +0530

    Update SecurityConfig.java

--- backend/src/main/java/com/antigravity/collaborativecoding/config/SecurityConfig.java
+++ backend/src/main/java/com/antigravity/collaborativecoding/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.antigravity.collaborativecoding.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
